67c2cb62c2d3bac5fb537fae34983aaab8f0f576
maria <mkotik456@gmail.com>
2025-07-05 02:00:23 +0300

    Added test for VSHOK

--- mew456/tests/test_vshok.py
+++ mew456/tests/test_vshok.py
@@ -1,2 +1,23 @@
-mew
+import pytest
+import requests
 
+BASE_URL = "https://yavshok.ru"
+CORRECT_EMAIL = "test@example.com"
+INCORRECT_EMAIL = "newtest@example.com"
+
+
+def test_vshok_successful():
+    response = requests.post(
+        f"{BASE_URL}",
+        json={"email": CORRECT_EMAIL},
+    )
+    assert response.status_code == 200
+    assert "token" in response.json()
+
+
+def test_vshok_fail():
+    response = requests.post(
+        f"{BASE_URL}",
+        json={"email": INCORRECT_EMAIL},
+    )
+    assert response.status_code == 401
