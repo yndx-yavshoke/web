8f3ccad259f91478f33ffc57dfea85e180d4e545
maria <mkotik456@gmail.com>
2025-07-05 03:48:36 +0300

    Fixed bugs in test_name_changing.py

--- mew456/tests/test_name_changing.py
+++ mew456/tests/test_name_changing.py
@@ -1,24 +1,29 @@
 import pytest
 import requests
 
-BASE_URL = "https://yavshok.ru"
-TEST_EMAIL = "test@example.com"
-CORRECT_PASSWORD = "123456"
+BASE_URL = "http://localhost:3000"
+CORRECT_EMAIL = "user@example.com"
+CORRECT_PASSWORD = "string"
 
 
 def test_name_changing():
     login_response = requests.post(
-        f"{BASE_URL}/login",
-        json={"email": TEST_EMAIL, "password": CORRECT_PASSWORD},
+        f"{BASE_URL}/auth/login",
+        json={"email": CORRECT_EMAIL, "password": CORRECT_PASSWORD},
     )
+
     assert login_response.status_code == 200
+    assert "token" in login_response.json()
     token = login_response.json()["token"]
-    
+
     headers = {"Authorization": f"Bearer {token}"}
-    
+
     update_data = {"name": "New Name"}
-    update_response = requests.patch(f"{BASE_URL}/edit?__EXPO_ROUTER_key=undefined-U8oKLQOgMTwTay3spXV8F", json=update_data, headers=headers)
+    update_response = requests.patch(
+        f"{BASE_URL}/user/name",
+        json=update_data,
+        headers=headers,
+    )
     assert update_response.status_code == 200
-    
-    user_response = requests.get(f"{BASE_URL}/edit?__EXPO_ROUTER_key=undefined-U8oKLQOgMTwTay3spXV8F", headers=headers)
-    assert user_response.json()["name"] == "New Name"    
+
+    response_json = update_response.json()
