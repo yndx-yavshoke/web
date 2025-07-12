fefa5311fae52ccfc4a97109959fc3272839f015
Tatiana Mantrova <tatiana.mantrova@gmail.com>
2025-07-12 17:21:49 +0300

    create testplane tests

---
+++ tatiana_mantrova/testplane-project/config/test-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    BASE_URL: 'https://yavshok.ru/',
+
+    CREDENTIALS: {
+        email: 'test@example.com',
+        password: 'password123'
+    },
+
+    MY_CREDENTIALS: {
+        email: 'tatiana.mantrova@gmail.com',
+        password: 'qwdqwdqw'
+    },
+
+    SCREENSHOT_OPTIONS: {
+        disableAnimation: true,
+        ignoreElements: ['.live-counter', 'img[src*=".gif"]'],
+        allowViewportOverflow: true
+    }
+}
---
+++ tatiana_mantrova/testplane-project/testplane.config.ts
@@ -0,0 +1,42 @@
+export default {
+    gridUrl: "local",
+    baseUrl: "http://localhost",
+    pageLoadTimeout: 0,
+    httpTimeout: 60000,
+    testTimeout: 90000,
+    resetCursor: false,
+    sets: {
+        desktop: {
+            files: [
+                "testplane-tests/**/*.testplane.(t|j)s"
+            ],
+            browsers: [
+                "chrome",
+                "firefox"
+            ]
+        }
+    },
+    browsers: {
+        chrome: {
+            headless: true,
+            desiredCapabilities: {
+                browserName: "chrome"
+            }
+        },
+        firefox: {
+            headless: true,
+            desiredCapabilities: {
+                browserName: "firefox"
+            }
+        }
+    },
+    plugins: {
+        "html-reporter/testplane": {
+            // https://github.com/gemini-testing/html-reporter
+            enabled: true,
+            path: "testplane-report",
+            defaultView: "all",
+            diffMode: "3-up-scaled"
+        }
+    }
+};
