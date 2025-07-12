4850ec8d1ed25ec4142a33cc129e59c065c7369f
kirillmusenko <mushenkoka@icloud.com>
2025-07-12 11:33:09 -0700

    "add autotests on JS with TestPlane"
    remove testplane-report
    
    change file

---
+++ @Lumenessence/TestPlane/testplane-tests/editprofile.testplane.ts
@@ -0,0 +1,45 @@
+describe('Скриншотные тесты страницы Редактирование профиля', function () {
+    const config = require('../config/test-config')
+
+    const login = async function ({ browser }) {
+        await browser.url(config.BASE_URL + 'login')
+
+        await browser
+            .$('[data-testid="login-email-input"]')
+            .setValue(config.MY_CREDENTIALS.email)
+
+        await browser
+            .$('[data-testid="login-password-input"]')
+            .setValue(config.MY_CREDENTIALS.password)
+
+        await browser
+            .$('[data-testid="login-submit-button"]')
+            .click()
+
+        await browser.waitUntil(
+            async () => {
+                const url = await browser.getUrl();
+                return url.includes('/');
+            },
+            {
+                timeout: 5000,
+                timeoutMsg: 'Не удалось авторизоваться в течение 5 секунд'
+            }
+        )
+    }
+
+    beforeEach(async function ({ browser }) {
+        await login({ browser })
+        await browser
+            .$('[data-testid="user-edit-profile-button"]')
+            .click()
+    })
+
+    it('Страница редактирования профиля', async function ({ browser }) {
+        await browser.assertView(
+            'edit_profile_page',
+            'body',
+            config.SCREENSHOT_OPTIONS
+        )
+    })
+})
---
+++ @Lumenessence/TestPlane/testplane-tests/profile.testplane.ts
@@ -0,0 +1,53 @@
+describe('Скриншотные тесты страницы Профиля', function () {
+    const config = require('../config/test-config')
+
+    const CREDENTIALS = {
+        email: '733@733.ru',
+        password: '733733'
+    };
+
+    const login = async function ({ browser }) {
+        await browser.url(config.BASE_URL + 'login');
+
+        await browser
+            .$('[data-testid="login-email-input"]')
+            .setValue(CREDENTIALS.email)
+
+        await browser
+            .$('[data-testid="login-password-input"]')
+            .setValue(CREDENTIALS.password)
+
+        await browser
+            .$('[data-testid="login-submit-button"]')
+            .click()
+
+        await browser.waitUntil(
+            async () => {
+                const url = await browser.getUrl();
+                return url.includes('/');
+            },
+            {
+                timeout: 5000,
+                timeoutMsg: 'Не удалось авторизоваться в течение 5 секунд'
+            }
+        )
+    }
+
+    it('Блок статистики профиля', async function ({ browser }) {
+        await login({ browser })
+        await browser.assertView(
+            'user_page',
+            '.css-175oi2r.r-18u37iz.r-a2tzq0',
+            config.SCREENSHOT_OPTIONS
+        );
+    })
+
+    it('Блок шапки профиля', async function ({ browser }) {
+        await login({ browser })
+        await browser.assertView(
+            'user_page',
+            '.css-175oi2r.r-1habvwh.r-18u37iz',
+            config.SCREENSHOT_OPTIONS
+        )
+    })
+})
