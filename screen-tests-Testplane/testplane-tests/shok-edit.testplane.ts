f5d346c51a395388bedcbc206b9c1bc3dd7ad1d4
Nikita Chernousov <nchern899@gmail.com>
2025-07-13 22:39:49 +0300

    [Typescript/Testplane] Added Screenshot tests

---
+++ screen-tests-Testplane/testplane-tests/shok-edit.testplane.ts
@@ -0,0 +1,56 @@
+import { login } from '../testplane-helpers/auth';
+import { selectors } from '../testplane-helpers/selectors';
+
+describe("Edit page", () => {
+    
+    beforeEach(async ({ browser }) => {
+        await browser.setWindowSize(1920, 1080);
+
+        await login(browser);
+
+        const editButton = await browser.$(selectors.editButton);
+        await editButton.click();
+    });
+
+    
+    it("should have empty fields state", async ({ browser }) => {
+        const nameInput = await browser.$(selectors.nameInput);
+        await nameInput.clearValue();
+
+        await browser.assertView('edit-page-empty-name', 'body', { 
+            screenshotDelay: 1000 
+        });
+    });
+
+    it("should display Name change field", async ({ browser }) => {
+        const nameInput = await browser.$(selectors.nameInput);
+        await nameInput.clearValue();
+
+        await browser.assertView('edit-name-change-field', selectors.nameInput, {
+            screenshotDelay: 500
+        });
+    });
+
+    it("should focus on Name change field", async ({ browser }) => {
+        const nameInput = await browser.$(selectors.nameInput);
+        await nameInput.clearValue();
+        await nameInput.click();
+
+        await browser.assertView('edit-focused-name-change-field', selectors.nameInput, {
+            screenshotDelay: 500
+        });
+    });
+
+    it("should display Save Changes button", async ({ browser }) => {
+        await browser.assertView('edit-save-button', selectors.saveButton, {
+            screenshotDelay: 500
+        });
+    });
+
+    it("should display Cancel button", async ({ browser }) => {
+        await browser.assertView('edit-cancel-button', selectors.backToProfileButton, {
+            screenshotDelay: 500
+        });
+    });
+
+});
