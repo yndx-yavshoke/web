a1065e3b146ff3e2bf8f79d1f16e97b415823d86
Aleksandr <al.colesnicov@gmail.com>
2025-07-13 22:27:47 +0300

    Отчеты и обновленные тесты шагами и проверками

--- .expo/Projects/tests/pages/base.page.ts
+++
@@ -1,9 +0,0 @@
-import { Page,Locator}  from "@playwright/test";
-import { strict } from "assert";
-
-export class BasePage{
-    constructor(public readonly page:Page){}
-        async open(path:string){
-        await this.page.goto(path)
-    }
-}
