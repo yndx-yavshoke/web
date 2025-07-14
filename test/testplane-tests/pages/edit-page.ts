744c871e505d9ad54438b104b5e8df9d0041c29b
Elizaveta Logvinenko <elizaveta20logvinenko@gmail.com>
2025-07-14 15:03:56 +0400

    fixed some errors

--- test/testplane-tests/pages/edit-page.ts
+++ test/testplane-tests/pages/edit-page.ts
@@ -9,22 +9,6 @@ export class EditPage {
     }
 
     get title() {
-        return this.browser.$('[class="css-146c3p1 r-vw2c0b r-1yflyrw r-q4m81j r-1ui5ee8"]');
-    }
-
-    get nameInput() {
-        return this.browser.$(this.selectors.nameInput);
-    }
-
-    get buttons() {
-        return this.browser.$('[class="css-175oi2r r-1ssbvtb r-1x0uki6"]');
-    }
-
-    get () {
-        return this.browser.$('[data-testid="edit-save-button"]');
-    }
-
-    get cancelButton() {
-        return this.browser.$('[data-testid="edit-cancel-button"]');
+        return this.browser.$('//div[text()="Edit Profile"]');
     }
 }
