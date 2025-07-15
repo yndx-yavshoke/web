8904ba6f3d1c4ef2c5569120b8905a6706fcc645
DmitriyL <scarium@yandex.ru>
2025-07-15 11:08:46 +0300

    Add TestPlane screenshot tests

---
+++ testplane/testplane-tests/login.testplane.ts
@@ -0,0 +1,108 @@
+import { selectors } from '../testplane-auth/selectors';
+
+describe("Login page", () => {
+
+    beforeEach(async ({ browser }) => {
+        
+        await browser.openAndWait("/login");
+    });
+
+
+    it("should have default page state", async ({ browser }) => {
+        await browser.assertView('login-page-default', 'body', {
+            screenshotDelay: 300
+        });
+    });
+
+    it("should display Email input field", async ({ browser }) => {
+        await browser.assertView('login-email-field', selectors.emailInput, {
+            screenshotDelay: 500
+        });
+    });
+
+    it("should focus on Email input field", async ({ browser }) => {
+        const inputEmail = await browser.$(selectors.emailInput);
+        await inputEmail.click();
+
+        await browser.assertView('login-focused-email-field', selectors.emailInput, {
+            screenshotDelay: 500
+        });
+    });
+
+    it("should display Password input field", async ({ browser }) => {
+        await browser.assertView('login-password-field', selectors.passwordInput, {
+            screenshotDelay: 500
+        });
+    });
+
+    it("should focus on Password input field", async ({ browser }) => {
+        const inputPassword = await browser.$(selectors.passwordInput);
+        await inputPassword.click();
+
+        await browser.assertView('login-focused-password-field', selectors.passwordInput, {
+            screenshotDelay: 500
+        });
+    });
+
+    it("should display Login submit button", async ({ browser }) => {
+        await browser.assertView('login-submit-button', selectors.loginButton, {
+            screenshotDelay: 500
+        });
+    });
+
+    it("should display back to Main button", async ({ browser }) => {
+        await browser.assertView('login-back-button', selectors.backToMainButton, {
+            screenshotDelay: 500
+        });
+    });
+
+    it("should display to Register button", async ({ browser }) => {
+        await browser.assertView('login-to-register-button', selectors.toRegisterButton, {
+            screenshotDelay: 500
+        });
+    });
+
+    it("should throw error (Empty fields)", async ({ browser }) => {
+        const loginButton = await browser.$(selectors.loginButton);
+        await loginButton.click();
+
+        await browser.assertView('login-empty-fields-error', 'body', {
+            screenshotDelay: 1000
+        });
+    });
+
+    it("should throw error (Login error)", async ({ browser }) => {
+        const inputEmail = await browser.$(selectors.emailInput);
+        const inputPassword = await browser.$(selectors.passwordInput);
+        const loginButton = await browser.$(selectors.loginButton);
+
+        await inputEmail.setValue('w');
+        await inputPassword.setValue('w');
+        await loginButton.click();
+
+        await inputEmail.clearValue();
+        await inputPassword.clearValue();
+
+        await browser.assertView('login-error', 'body', {
+            screenshotDelay: 5000
+        });
+    });
+
+    it("should throw error (Incorrect credentials)", async ({ browser }) => {
+        const inputEmail = await browser.$(selectors.emailInput);
+        const inputPassword = await browser.$(selectors.passwordInput);
+        const loginButton = await browser.$(selectors.loginButton);
+
+        await inputEmail.setValue('21non_exist23@ya.ru');
+        await inputPassword.setValue('43www222');
+        await loginButton.click();
+
+        await inputEmail.clearValue();
+        await inputPassword.clearValue();
+
+        await browser.assertView('login-incorrect-credentials', 'body', {
+            screenshotDelay: 1000
+        });
+    });
+
+});
---
+++ testplane/testplane-tests/profile.testplane.ts
@@ -0,0 +1,39 @@
+import { login } from '../testplane-auth/auth';
+import { selectors } from '../testplane-auth/selectors';
+
+describe("Profile page", () => {
+
+    beforeEach(async ({ browser }) => {
+        
+        await login(browser);
+    });
+
+
+    it("should have default page state", async ({ browser }) => {
+        await browser.assertView('profile-page-default', 'body', {
+            ignoreElements: [
+                'div.css-146c3p1.r-vw2c0b',
+                'div.css-146c3p1.r-1khnkhu',
+                'img[src*="profile.4c9412d0fd7b6d90111faab09c8f6c4a.gif"]',
+                'img[src*="1.270a55bb6732b03a7ba9e1b8576e334e.jpg"]',
+                'img[src*="2.ac5aa59c18630318c9c527d1e6fc5664.webp"]',
+                'img[src*="3.0891d9bfad47fd7792dd1328a4b9dbef.jpg"]',
+                'img[src*="4.97dfdf629984d4c2f78975152341dbbe.jpg"]'
+            ],
+            screenshotDelay: 1000
+        });
+    });
+
+    it("should display Edit button", async ({ browser }) => {
+        await browser.assertView('profile-edit-button', selectors.editButton, {
+            screenshotDelay: 500
+        });
+    });
+
+    it("should display Logout button", async ({ browser }) => {
+        await browser.assertView('profile-logout-button', selectors.logoutButton, {
+            screenshotDelay: 500
+        });
+    });
+
+});
